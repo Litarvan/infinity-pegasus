90b0bceb9049e8163a6511313db0edfa64ab3328
Adrien Navratil <adrien1975@live.fr>
2022-03-31 04:17:18 +0200

    Updated TODOs

--- src/app.js
+++ src/app.js
@@ -8,8 +8,10 @@ export function title(page) {
     return `${app.name} — ${page}`;
 }
 
+// TODO: Coefficients
+// TODO: Progression
 // TODO: Moyenne du module ?
 // TODO: Modal pour les conditions d'utilisation
-// TODO: Coefficients
-// TODO: Vite fait improve le design (mettre en grand si ya qu'une note ?) (tout réduire ?)
 // TODO: Indiquer qu'on peut activer/désactiver l'extension via l'icône en haut à droite
+
+// TODO: Vite fait improve le design (mettre en grand si ya qu'une note ?) (tout réduire ?)
