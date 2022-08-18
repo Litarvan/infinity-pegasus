7d2ddeb8f050e1394ccbd4a94109c953d99ecb5d
Adrien Merat <adrien.merat@epita.fr>
2022-08-18 10:40:12 +0200

    [ADD] MCE8 module in `s8_SCIA_2023.js`

---
+++ src/lib/pegasus/coefficients/s8_SCIA_2023.js
@@ -0,0 +1,31 @@
+export default {
+    MCE8: {
+        COINM: {
+            _subject: 1/7
+        },
+        DBRE: {
+            _subject: 1/7
+        },
+        DPES: {
+            _subject: 1/14,
+            'DPES_NOTE_TD_COLLABORATION': 1/3,
+            'DPES_NOTE_OBJECTIF': 1/3,
+            'DPES_NOTE_VIDEO': 1/3
+        },
+        M_CRIN: {
+            _subject: 1/7
+        },
+        MPRO: {
+            _subject: 1/7
+        },
+        PFE_SCIA: {
+            _subject: 1/7
+        },
+        PIFI: {
+            _subject: 1/7
+        },
+        RE_S8: {
+            _subject: 1/14
+        }
+    }
+}
